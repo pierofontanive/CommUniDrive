c4c31a7e94b69e7036b2525be677fe8a2c16f7b1
pierofontanive <piero.fontanive@gmail.com>
2023-04-25 14:04:28 +0200

    Aggiunta implementazione del menu "drawer" con delle pagine di esempio

--- app/src/main/java/com/example/communidrive/MainActivity.java
+++ app/src/main/java/com/example/communidrive/MainActivity.java
@@ -1,16 +1,78 @@
 package com.example.communidrive;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_1()).commit();
+            navigationView.setCheckedItem(R.id.nav_1);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_1()).commit();
+                break;
+            case R.id.nav_2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_2()).commit();
+                break;
+            case R.id.nav_3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment_3()).commit();
+                break;
+            case R.id.nav_4:
+                Toast.makeText(this, "Test4", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_5:
+                Toast.makeText(this, "Test5", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
